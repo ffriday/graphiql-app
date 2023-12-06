ac9be785f084a51bdb78cd3bb7acef54bef83eda
mitrich <4irtim@gmail.com>
2023-12-06 22:22:42 +0300

    feat: add styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import styled, { css } from "styled-components";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+const Button = styled.button<{ $primary?: boolean }>`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid #bf4f74;
+  color: #bf4f74;
+  margin: 0.5em 1em;
+  padding: 0.25em 1em;
+
+  ${(props) =>
+    props.$primary &&
+    css`
+      background: #bf4f74;
+      color: white;
+    `}
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -28,8 +45,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Button>Styled components test normal</Button>
+      <Button $primary>Styled components test primary</Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
