02793dc5fe8ba74b9dc85dc48c7f1333cf9fa182
RomanKadevich <123977364+RomanKadevich@users.noreply.github.com>
2023-12-09 18:10:00 +0300

    feat: sign in and sign up

--- src/pages/signIn-page/sign-in.tsx
+++ src/pages/signIn-page/sign-in.tsx
@@ -1,12 +1,50 @@
 import { LANGUAGES, lang } from "../../constants/lang";
 import { useAppContext } from "../../provisers/LangProvider";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import ErrorInfo from "../../components/FormErrorInfo/ErrorInfo";
+import styles from "./sign-in.module.scss";
+import { signInSchema } from "../../constants/signInValidation";
+
+interface ISignInData {
+  email: string;
+  password: string;
+}
 
 export const SignInPage = () => {
   const { language } = useAppContext();
   const { signIn } = lang[language as keyof typeof LANGUAGES];
+  const {
+    register,
+    formState: { errors, isValid },
+    handleSubmit,
+    reset,
+  } = useForm({
+    mode: "onChange",
+    resolver: yupResolver(signInSchema),
+  });
+  const onSubmit = (data: ISignInData) => {
+    console.log(data);
+    reset();
+  };
+
   return (
-    <>
-      <h1>{signIn}</h1>
-    </>
+    <div className="container-auth">
+      <h2>{signIn}</h2>
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+        <input placeholder="E-mail" {...register("email")} />
+        <ErrorInfo errors={errors.email} />
+        <input
+          type="password"
+          placeholder="Password"
+          {...register("password")}
+        />
+        <ErrorInfo errors={errors.password} />
+        <button type="submit" disabled={!isValid}>
+          {signIn}
+        </button>
+        <button type="button"> Google </button>
+      </form>
+    </div>
   );
 };
--- src/pages/signUp-page/sign-up.tsx
+++ src/pages/signUp-page/sign-up.tsx
@@ -1,12 +1,55 @@
 import { LANGUAGES, lang } from "../../constants/lang";
 import { useAppContext } from "../../provisers/LangProvider";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import ErrorInfo from "../../components/FormErrorInfo/ErrorInfo";
+import styles from "./sign-in.module.scss";
+import { signUpSchema } from "../../constants/signUpValidation";
 
+interface ISignUpData {
+  email: string;
+  password: string;
+  anotherPassword: string;
+}
 export const SignUpPage = () => {
   const { language } = useAppContext();
   const { signUp } = lang[language as keyof typeof LANGUAGES];
+  const {
+    register,
+    formState: { errors, isValid },
+    handleSubmit,
+    reset,
+  } = useForm({
+    mode: "onChange",
+    resolver: yupResolver(signUpSchema),
+  });
+  const onSubmit = (data: ISignUpData) => {
+    console.log(data);
+    reset();
+  };
+
   return (
-    <>
-      <h1>{signUp}</h1>
-    </>
+    <div className="container-auth">
+      <h2>{signUp}</h2>
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+        <input placeholder="E-mail" {...register("email")} />
+        <ErrorInfo errors={errors.email} />
+        <input
+          type="password"
+          placeholder="Password"
+          {...register("password")}
+        />
+        <ErrorInfo errors={errors.password} />
+        <input
+          type="password"
+          placeholder="Password"
+          {...register("anotherPassword")}
+        />
+        <ErrorInfo errors={errors.anotherPassword} />
+        <button type="submit" disabled={!isValid}>
+          {signUp}
+        </button>
+      </form>
+    </div>
   );
 };
