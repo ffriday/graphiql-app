4394b46044765833dae1e915b97c35e02e771186
mitrich <4irtim@gmail.com>
2023-12-12 11:40:42 +0300

    feat: add clean anc chesk code functions

--- src/pages/graphiQL-page/graphiQL.tsx
+++ src/pages/graphiQL-page/graphiQL.tsx
@@ -14,10 +14,55 @@ export const GraphiQLPage = () => {
       <div className="graphiQL-container">
         <aside className="settings"></aside>
         <main className="session">
-          <div className="editor"></div>
+          <Editor />
           <div className="response"></div>
         </main>
       </div>
     </>
   );
+};
+
+const graphiQLText = `
+# Welcome to GraphiQL
+#
+# GraphiQL is an in-browser tool for writing, validating, and
+# testing GraphQL queries.
+
+query test (if: Int) {
+  allFilms {
+    edges {
+      node {
+        id
+      }
+    }
+  }
+}`;
+
+const prettify = (code: string) => {
+  const lines = cleanCode(code).join("");
+  const valid = checkCode(lines);
+  const res = formatCode(lines);
+  return { valid, res };
+};
+
+const formatCode = (code: string) => {
+  return code;
+};
+
+const cleanCode = (code: string) =>
+  code
+    .split(/\r?\n/)
+    .filter((line) => line.trim().length > 0 && !line.trim().startsWith("#"));
+
+const checkCode = (code: string) =>
+  code.split("").reduce((acc, char) => {
+    if (char === "{") acc++;
+    if (char === "}") acc = acc > 0 ? acc - 1 : -Infinity;
+    return acc;
+  }, 0) === 0;
+
+console.log(prettify(graphiQLText));
+
+const Editor = () => {
+  return <pre className="editor">{graphiQLText}</pre>;
 };
