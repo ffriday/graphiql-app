4d269b758baa1654a3abbe7fd40289abc16db18d
mitrich <4irtim@gmail.com>
2023-12-15 16:33:47 +0300

    feat: add codemirror input element

--- src/components/editor/editor.tsx
+++ src/components/editor/editor.tsx
@@ -1,5 +1,7 @@
+import CodeMirror from "@uiw/react-codemirror";
 import { useQueryContext } from "../../providers/queryProvider";
 import { parseCode } from "./prettify";
+import { useCallback } from "react";
 
 export const Editor = () => {
   const {
@@ -9,17 +11,16 @@ export const Editor = () => {
     updateData,
   } = useQueryContext();
 
-  const changeQuery = ({
-    currentTarget: { textContent },
-  }: React.FormEvent<HTMLPreElement>) => {
-    const value = textContent ?? "";
-    const { isValid } = parseCode(value);
-    updateData({ query: { value, isValid } });
-  };
+  const onChange = useCallback(
+    (value: string) => {
+      const val = value ?? "";
+      const { isValid } = parseCode(val);
+      updateData({ query: { value: val, isValid } });
+    },
+    [updateData],
+  );
 
   return (
-    <pre className="editor" contentEditable="true" onInput={changeQuery}>
-      {value}
-    </pre>
+    <CodeMirror value={value} width="100%" height="100%" onChange={onChange} />
   );
 };
