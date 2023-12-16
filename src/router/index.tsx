e5812ff1a900323025187417c18b9378ea3e1e11
Ekaterina Kiryanova <109188496+KrKate@users.noreply.github.com>
2023-12-16 22:57:38 +0500

    fix: change errorElements

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -8,8 +8,8 @@ import { ErrorPage } from "../pages/error-page/error";
 import { SignInPage } from "../pages/signIn-page/sign-in";
 import { WelcomePage } from "../pages/welcome-page/welcome";
 import { APP_ROUTES } from "../constants/constants";
-import { RootLayout } from "../layouts/Root-layout";
 import { GraphiQLPage } from "../pages/graphiQL-page/graphiQL";
+import { RootLayout } from "../layouts/root-layout";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
@@ -17,22 +17,22 @@ export const router = createBrowserRouter(
       <Route
         path={APP_ROUTES.SIGNIN}
         element={<SignInPage />}
-        errorElement={<SignInPage />}
+        errorElement={<ErrorPage />}
       />
       <Route
         path={APP_ROUTES.SIGNUP}
         element={<SignUpPage />}
-        errorElement={<SignUpPage />}
+        errorElement={<ErrorPage />}
       />
       <Route
         path={APP_ROUTES.GRAPHIQL}
         element={<GraphiQLPage />}
-        errorElement={<GraphiQLPage />}
+        errorElement={<ErrorPage />}
       />
       <Route
         path={APP_ROUTES.WELCOME}
         element={<WelcomePage />}
-        errorElement={<WelcomePage />}
+        errorElement={<ErrorPage />}
       />
       <Route path="/*" element={<ErrorPage />} errorElement={<WelcomePage />} />
     </Route>,
