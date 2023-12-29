a5af22f47cd1e361d4ba2df9106e96ce11e02603
mitrich <4irtim@gmail.com>
2023-12-29 15:33:42 +0300

    refactor: prettify function

--- src/functions/prettify.ts
+++ src/functions/prettify.ts
@@ -2,46 +2,51 @@ import { Query } from "../constants";
 
 export const prettify = (code: string) => {
   const { value, isValid } = parseCode(code);
-  const query = isValid ? formatCode(value) : code;
-  return query;
+  if (!isValid) {
+    return code;
+  }
+  return formatCode(value);
 };
 
-const formatCode = (code: string, tabulation: number = 2) => {
-  const format = (code: string, level: number, result: string[]) => {
-    const open = code.indexOf("{");
-    const closed = code.indexOf("}");
-    if (closed < 0) return;
-    const isOpen = open >= 0 && open < closed;
-    const position = isOpen ? open : closed;
-    let tab = " ".repeat(level * tabulation);
-    let line = code.substring(0, position).trim();
+const formatCode = (value: string) => {
+  const code = cleanCode(value);
+  const tab = "  ";
 
-    if (isOpen) {
-      line = line
-        .split(" ")
-        .filter((word) => word.length > 0)
-        .map((word) => word.trim())
-        .join(" ");
-      line = line.replaceAll("( ", "(");
-      line = line.replaceAll(" )", ")");
+  const lines = code
+    .split(/[{}]+/)
+    .map((line) => line.replace("/n", "").trim());
+
+  let indent = 0;
+  const indents = code.split("").reduce<[number, string][]>((acc, char) => {
+    if (char === "{") acc.push([indent++, "{"]);
+    if (char === "}") acc.push([indent--, "}"]);
+    return acc;
+  }, []);
+
+  const structuredLines = lines.map<[string, number, string]>((line, i) => {
+    if (indents[i]) {
+      return [line, indents[i][0], indents[i][1]];
     } else {
-      if (line.length > 1) {
-        line = `${line
-          .split("\n")
-          .map((line) => line.trim())
-          .join(`\n${tab}`)}\n${tab.substring(tabulation)}`;
-      } else {
-        tab = tab.substring(tabulation);
-      }
+      return [line, 0, ""];
     }
+  });
 
-    result.push(`${tab}${line}${isOpen ? " {" : "}"}`);
-    format(code.substring(position + 1), level + (isOpen ? 1 : -1), result);
-  };
+  const res = structuredLines
+    .reduce<string[]>((acc, [text, indent, type]) => {
+      const line = text
+        .split(/\r?\n/)
+        .map((line) => `${tab.repeat(indent)}${line}`)
+        .join("\n");
+      if (type === "{") acc.push(`${line} {\n`);
+      if (type === "}") {
+        if (line.trim()) acc.push(`${line}\n`);
+        acc.push(`${tab.repeat(indent - 1)}}\n`);
+      }
+      return acc;
+    }, [])
+    .join("");
 
-  const result: string[] = [];
-  format(code, 0, result);
-  return result.join("\n").trim();
+  return res;
 };
 
 export const parseCode = (code: string): Query => {
@@ -52,6 +57,9 @@ export const parseCode = (code: string): Query => {
 
 const cleanCode = (code: string) =>
   code
+    .replaceAll(/[ \t]+/g, " ")
+    .replaceAll("( ", "(")
+    .replaceAll(" )", ")")
     .split(/\r?\n/)
     .filter((line) => line.trim().length > 0 && !line.trim().startsWith("#"))
     .map((line) => line.trim())
