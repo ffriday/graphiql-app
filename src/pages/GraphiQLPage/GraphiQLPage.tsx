4fc8f7bb3daf0cfc9ef9a5a54056de72a91f1327
mitrich <4irtim@gmail.com>
2024-01-05 16:37:17 +0300

    feat: add endpoint input

--- src/pages/GraphiQLPage/GraphiQLPage.tsx
+++ src/pages/GraphiQLPage/GraphiQLPage.tsx
@@ -1,23 +1,45 @@
 import { useContext } from "react";
 import { AuthContext } from "../../providers/AuthProviders";
-import { APP_ROUTES, LangPages } from "../../constants/constants";
+import {
+  APP_ROUTES,
+  INITIAL_ENDPOINT,
+  LangPages,
+  ParamKeys,
+} from "../../constants/constants";
 import { SignInPage } from "../SignInPage/SignInPage";
 import { Editor } from "./Editor";
 import { useLoadQuery, useRedirect, useTranslate } from "../../hooks";
 import CodeMirror from "@uiw/react-codemirror";
+import { Input, InputLabel } from "@mui/material";
 import styles from "./GraphiQLPage.module.scss";
+import { useSearchParams } from "react-router-dom";
 
 export const GraphiQLPage = () => {
   const translate = useTranslate(LangPages.graphql);
   const { userId } = useContext(AuthContext);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const endpoint = searchParams.get(ParamKeys.endpoint) ?? INITIAL_ENDPOINT;
 
   useRedirect(`/${APP_ROUTES.SIGNIN}`, null);
   useLoadQuery(userId);
 
+  const onChange = (value: string, key: ParamKeys) => {
+    searchParams.set(key, value);
+    setSearchParams(searchParams);
+    window.localStorage.setItem(key, value);
+  };
+
   return userId ? (
     <>
       <h1>GraphQL</h1>
-      <p>{translate("graphQLMessage")}</p>
+      <div className={styles.endpoint}>
+        <InputLabel>{translate("endpoint")}</InputLabel>
+        <Input
+          className={styles.endpointInput}
+          value={endpoint}
+          onChange={(event) => onChange(event.target.value, ParamKeys.endpoint)}
+        />
+      </div>
       <div className={styles.graphQLcontainer}>
         <main className={styles.session}>
           <Editor />
