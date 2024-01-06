20cc5a94a80ecd22f0d95162d8aa0dc5061af690
Ekaterina Kiryanova <109188496+KrKate@users.noreply.github.com>
2024-01-06 19:46:28 +0500

    feat: transfer schema functionality

---
+++ src/components/Schema/Schema.tsx
@@ -0,0 +1,169 @@
+// Статья с описанием получения схемы: https://medium.com/@mrthankyou/how-to-get-a-graphql-schema-28915025de0e
+import { useEffect, useState } from "react";
+import styles from "./Schema.module.scss";
+import { queryAll as query } from "./constants";
+
+interface Type {
+  name: string;
+  ofType?: Type;
+  fields?: Field[];
+}
+
+interface Field {
+  name: string;
+  type: Type;
+}
+
+interface TypeItemProps {
+  type: Type;
+}
+
+export const endpoint =
+  "https://swapi-graphql.netlify.app/.netlify/functions/index";
+
+function Schema(): JSX.Element {
+  type SchemaData = {
+    __schema: {
+      types: {
+        name: string;
+        fields: {
+          name: string;
+          args: {
+            name: string;
+            type: {
+              name: string;
+            };
+          }[];
+          type: {
+            name: string;
+          };
+          isDeprecated: boolean;
+          deprecationReason: string | null;
+        }[];
+      }[];
+    };
+  };
+
+  function renderNestedTypes(type: Type) {
+    if (!type.ofType) {
+      return <span>{type.name}</span>;
+    }
+    return (
+      <div>
+        <div>
+          <a href="#">{type.name}</a>
+        </div>
+        {type.fields && (
+          <ul className={styles.fieldsList}>
+            {type.fields.map((field) => (
+              <li key={field.name}>
+                <span className={styles.fieldName}>{field.name}:</span>
+                {renderNestedTypes(field.type)}
+              </li>
+            ))}
+          </ul>
+        )}
+        {renderNestedTypes(type.ofType)}
+      </div>
+    );
+  }
+
+  function TypeItem({ type }: TypeItemProps) {
+    return (
+      <div>
+        <a href="#">{type.name}</a>
+        {type.fields && (
+          <ul className={styles.fieldsList}>
+            {type.fields.map((field) => (
+              <li key={field.name}>
+                <span className={styles.fieldName}>{field.name}:</span>
+                {renderNestedTypes(field.type)}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    );
+  }
+
+  const [schemaData, setSchemaData] = useState<SchemaData | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(endpoint, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ query }),
+        });
+        const result = await response.json();
+        setSchemaData(result.data);
+      } catch (error) {
+        console.error("Error fetching schema:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className={styles.sсhemaContainer}>
+      <h1>Schema</h1>
+      {schemaData && (
+        <div>
+          <h4>Root Types</h4>
+          <div>
+            {schemaData.__schema.types
+              .filter((type) => type.name === "Root")
+              .map((type) => (
+                <div key={type.name}>
+                  {type.fields && (
+                    <ul className={styles.fieldsList}>
+                      {type.fields.map((field) => (
+                        <li key={field.name} className={styles.rootItem}>
+                          <span className={styles.fieldName}>
+                            {field.name}:
+                          </span>{" "}
+                          <span>
+                            (
+                            {field.args && (
+                              <>
+                                {field.args.map((arg) => (
+                                  <div key={arg.name}>
+                                    {arg.name}: {arg.type.name}
+                                  </div>
+                                ))}
+                              </>
+                            )}
+                            )
+                          </span>
+                          {" : "}
+                          <span>{field.type.name}</span>
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </div>
+              ))}
+          </div>
+
+          <h4>All Schema Types</h4>
+          <div>
+            {schemaData.__schema.types
+              .filter(
+                (type) =>
+                  !type.name.startsWith("__") && !type.name.startsWith("Root"),
+              )
+              .map((type) => (
+                <div key={type.name} className={styles.typeItem}>
+                  <TypeItem type={type} />
+                </div>
+              ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Schema;
--- src/pages/GraphiQLPage/Toolbar/Toolbar.tsx
+++ src/pages/GraphiQLPage/Toolbar/Toolbar.tsx
@@ -1,17 +1,21 @@
 import CleaningServicesOutlinedIcon from "@mui/icons-material/CleaningServices";
 import PlayCircleOutline from "@mui/icons-material/PlayCircleOutline";
 import DeleteForeverOutlined from "@mui/icons-material/DeleteForeverOutlined";
+import Description from "@mui/icons-material/Description";
 import { styles } from ".";
 import { IconButton } from "@mui/material";
 import { useTranslate } from "../../../hooks";
 import { checkCode, cleanCode, prettify } from "../../../functions";
 import { useSearchParams } from "react-router-dom";
 import { INITIAL_QUERY, LangPages, ParamKeys } from "../../../constants";
+import { useState } from "react";
+import Schema from "../../../components/Schema/Schema";
 
 export function Toolbar(): JSX.Element {
   const [searchParams, setSearchParams] = useSearchParams();
   const query = searchParams.get(ParamKeys.query) ?? "";
   const translate = useTranslate(LangPages.toolbar);
+  const [showSchema, setShowSchema] = useState(false);
 
   const prettifyCode = () => {
     searchParams.set(ParamKeys.query, prettify(query));
@@ -26,22 +30,33 @@ export function Toolbar(): JSX.Element {
     });
   };
 
+  const handleShowSchema = () => {
+    setShowSchema((prevShowSchema) => !prevShowSchema);
+  };
+
   return (
     <aside className={styles.Toolbar}>
-      <IconButton onClick={() => null} aria-label={translate("submit")}>
-        <PlayCircleOutline />
-      </IconButton>
+      <div className={styles.ToolbarButtons}>
+        <IconButton onClick={handleShowSchema} aria-label={translate("docs")}>
+          <Description />
+        </IconButton>
 
-      <IconButton
-        onClick={prettifyCode}
-        disabled={!checkCode(cleanCode(query))}
-        aria-label={translate("prettify")}
-      >
-        <CleaningServicesOutlinedIcon />
-      </IconButton>
-      <IconButton onClick={clear} aria-label={translate("clear")}>
-        <DeleteForeverOutlined />
-      </IconButton>
+        <IconButton onClick={() => null} aria-label={translate("submit")}>
+          <PlayCircleOutline />
+        </IconButton>
+
+        <IconButton
+          onClick={prettifyCode}
+          disabled={!checkCode(cleanCode(query))}
+          aria-label={translate("prettify")}
+        >
+          <CleaningServicesOutlinedIcon />
+        </IconButton>
+        <IconButton onClick={clear} aria-label={translate("clear")}>
+          <DeleteForeverOutlined />
+        </IconButton>
+      </div>
+      {showSchema && <Schema />}
     </aside>
   );
 }
